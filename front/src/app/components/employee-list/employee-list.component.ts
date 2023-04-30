e5d4365c1580a253f33440c81568b9a28fd22f14
AYOUB <a.idihia@gmail.com>
2023-04-30 21:27:01 +0200

    creating the employee list component and the service

--- front/src/app/components/employee-list/employee-list.component.ts
+++ front/src/app/components/employee-list/employee-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/employee';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit{
+  
+  employees: Employee[];
+
+  constructor(){
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
